1351f9e5be981e2069c97d7bb83df2b0ce7a62af
ZURIÑE BEHOBIDE ZABALEGUI <157796849+zuribehobide23@users.noreply.github.com>
2025-01-15 10:19:36 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let photos = document.querySelector("#image")
+
+let ArrayOfPhotos = ["url(images/1.jpg)", 
+    "url(images/2.jpg)", 
+    "url(images/3.jpg)", 
+    "url(images/4.jpg)",
+    "url(images/5.jpg)",
+    "url(images/6.jpg)",
+    "url(images/7.jpg)",
+    "url(images/8.jpg)",
+    "url(images/9.jpg)",
+    "url(images/10.jpg)",
+    "url(images/11.jpg)",
+    "url(images/12.jpg)",
+    "url(images/13.jpg)"]
+
+let index = 0;
+
+photos.style.backgroundImage = ArrayOfPhotos[index];
+
+function irudiakAldatu(){
+    index = (index + 1) % ArrayOfPhotos.length;
+    photos.style.backgroundImage = ArrayOfPhotos[index];  
+}
+let erloju = setInterval(irudiakAldatu, 5000);
